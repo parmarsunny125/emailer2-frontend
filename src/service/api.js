c903cad6e2569e543a8a1e1c1c1ad589f5924191
chandnijiv2001 <parmarsunny125@gmail.com>
2022-12-29 14:33:09 +0530

    up

--- src/service/api.js
+++ src/service/api.js
@@ -1,50 +1,58 @@
- import axios from 'axios'
-const URL='https://emailer2-api.onrender.com'
- export const addUser=async(data)=>{
-    try{
-        return await axios.post(`${URL}/add`,data)
-    }catch(error){
-        console.log('Api error');
-    }
- }
+import axios from 'axios';
 
- export const getUsers = async () =>{
-    try{
+const instance = axios.create({
+  baseURL: 'https://emailer2-api.onrender.com',
+  headers: {
+    Accept: 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('Token') || ''}`,
+  },
+});
 
-        return await axios.get(`${URL}/`)
-    }catch(error){
-        console.log(error);
-    }
- }
+export const addUser = async (data) => {
+  try {
+    return await instance.post('/add', data);
+  } catch (error) {
+    console.log('Api error');
+  }
+};
 
- export const getUser= async(id)=>{
-    try{
-        return await axios.get(`${URL}/${id}`)
-    }catch(error){
-        console.log(error);
-    }
- }
+export const getUsers = async () => {
+  try {
+    return await instance.get('/');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
- export const editUser= async(user,id)=>{
-    try{
-        return await axios.post(`${URL}/${id}`,user)
-    }catch(error){
-        console.log(error);
-    }
- }
- export const deleteUser= async(id)=>{
-    try{
-        return await axios.delete(`${URL}/${id}`)
-    }catch(error){
-        console.log(error);
-    }
- }
+export const getUser = async (id) => {
+  try {
+    return await instance.get(`/${id}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
- export const sendData = async (extractedData) => {
-    try {
-      const response = await axios.post(`${URL}/send-data`, { data:extractedData });
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+export const editUser = async (user, id) => {
+  try {
+    return await instance.post(`/${id}`, user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteUser = async (id) => {
+  try {
+    return await instance.delete(`/${id}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const sendData = async (extractedData) => {
+  try {
+    const response = await instance.post('/send-data', { data: extractedData });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
